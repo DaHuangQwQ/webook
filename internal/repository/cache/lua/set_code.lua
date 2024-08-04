abe1cdf220f093834e9f508c79e053fd45ee9bcf
DaHuangQwQ <2195921486@qq.com>
2024-08-04 22:12:49 +0800

    短信验证

---
+++ internal/repository/cache/lua/set_code.lua
@@ -0,0 +1,21 @@
+local key = KEYS[1]
+local cntKey = key..":cnt"
+-- 你准备的存储的验证码
+local val = ARGV[1]
+
+local ttl = tonumber(redis.call("ttl", key))
+if ttl == -1 then
+    --    key 存在，但是没有过期时间
+    return -2
+elseif ttl == -2 or ttl < 540 then
+    --    可以发验证码
+    redis.call("set", key, val)
+    -- 600 秒
+    redis.call("expire", key, 600)
+    redis.call("set", cntKey, 3)
+    redis.call("expire", cntKey, 600)
+    return 0
+else
+    -- 发送太频繁
+    return -1
+end
---
+++ internal/service/sms/tencent/service_test.go
@@ -0,0 +1,53 @@
+package tencent
+
+import (
+	"context"
+	"github.com/stretchr/testify/assert"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common"
+	"github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/common/profile"
+	sms "github.com/tencentcloud/tencentcloud-sdk-go/tencentcloud/sms/v20210111"
+	"os"
+	"testing"
+	mysms "webook/internal/service/sms"
+)
+
+func TestService_Send(t *testing.T) {
+	secretId, ok := os.LookupEnv("SMS_SECRET_ID")
+	if !ok {
+		t.Fatal()
+	}
+	secretKey, ok := os.LookupEnv("SMS_SECRET_KEY")
+
+	c, err := sms.NewClient(common.NewCredential(secretId, secretKey),
+		"ap-nanjing",
+		profile.NewClientProfile())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	s := NewService(c, "1400842696", "妙影科技")
+
+	var testCases = []struct {
+		name    string
+		tplId   string
+		params  []mysms.NamedArg
+		numbers []string
+		wantErr error
+	}{
+		{
+			name:  "发送验证码",
+			tplId: "1877556",
+			params: []mysms.NamedArg{
+				{Value: "123456"},
+			},
+			// 改成你的手机号码
+			numbers: []string{""},
+		},
+	}
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			er := s.Send(context.Background(), tc.tplId, tc.params, tc.numbers...)
+			assert.Equal(t, tc.wantErr, er)
+		})
+	}
+}
