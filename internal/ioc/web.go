dea1fb38e59628084e56dc213be991cd6536d211
DaHuangQwQ <2195921486@qq.com>
2024-08-05 16:01:24 +0800

    IOC 依赖注入

---
+++ internal/ioc/web.go
@@ -0,0 +1,47 @@
+package ioc
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+	"github.com/redis/go-redis/v9"
+	"strings"
+	"time"
+	"webook/internal/web"
+	"webook/internal/web/middleware"
+	"webook/pkg/ginx/middleware/ratelimit"
+)
+
+func InitWebServer(mdls []gin.HandlerFunc, userHdl *web.UserHandler) *gin.Engine {
+	server := gin.Default()
+	server.Use(mdls...)
+	userHdl.RegisterRoutes(server)
+	return server
+}
+
+func InitGinMiddlewares(redisClient redis.Cmdable) []gin.HandlerFunc {
+	return []gin.HandlerFunc{
+		cors.New(cors.Config{
+			//AllowAllOrigins: true,
+			//AllowOrigins:     []string{"http://localhost:3000"},
+			AllowCredentials: true,
+			AllowHeaders:     []string{"Content-Type", "Authorization"},
+			// 这个是允许前端访问你的后端响应中带的头部
+			ExposeHeaders: []string{"x-jwt-token"},
+			//AllowHeaders: []string{"content-type"},
+			//AllowMethods: []string{"POST"},
+			AllowOriginFunc: func(origin string) bool {
+				if strings.HasPrefix(origin, "http://localhost") {
+					//if strings.Contains(origin, "localhost") {
+					return true
+				}
+				return strings.Contains(origin, "your_company.com")
+			},
+			MaxAge: 12 * time.Hour,
+		}),
+		func(ctx *gin.Context) {
+			println("这是我的 Middleware")
+		},
+		ratelimit.NewBuilder(redisClient, time.Second, 1000).Build(),
+		(&middleware.LoginJwtMiddleware{}).Build(),
+	}
+}
