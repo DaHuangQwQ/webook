91756435f828f0d03fbf39d7d62a12b0357722cd
DaHuangQwQ <2195921486@qq.com>
2024-08-27 13:52:32 +0800

    长短token实现

---
+++ internal/web/jwt/redis.go
@@ -0,0 +1,128 @@
+package jwt
+
+import (
+	"errors"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
+	"github.com/redis/go-redis/v9"
+	"strings"
+	"time"
+)
+
+type RedisJWTHandler struct {
+	client        redis.Cmdable
+	signingMethod jwt.SigningMethod
+	rcExpiration  time.Duration
+}
+
+func NewRedisJWTHandler(client redis.Cmdable) Handler {
+	return &RedisJWTHandler{
+		client:        client,
+		signingMethod: jwt.SigningMethodHS512,
+		rcExpiration:  time.Hour * 24 * 7,
+	}
+}
+
+func (h *RedisJWTHandler) CheckSession(ctx *gin.Context, ssid string) error {
+	cnt, err := h.client.Exists(ctx, fmt.Sprintf("users:ssid:%s", ssid)).Result()
+	switch err {
+	case redis.Nil:
+		return nil
+	case nil:
+		if cnt == 0 {
+			return nil
+		}
+		return errors.New("session 无效")
+	default:
+		return err
+	}
+}
+
+// ExtractToken 根据约定，token 在 Authorization 头部
+// Bearer XXXX
+func (h *RedisJWTHandler) ExtractToken(ctx *gin.Context) string {
+	authCode := ctx.GetHeader("Authorization")
+	if authCode == "" {
+		return authCode
+	}
+	segs := strings.Split(authCode, " ")
+	if len(segs) != 2 {
+		return ""
+	}
+	return segs[1]
+}
+
+var _ Handler = &RedisJWTHandler{}
+
+func (h *RedisJWTHandler) SetLoginToken(ctx *gin.Context, uid int64) error {
+	ssid := uuid.New().String()
+	err := h.setRefreshToken(ctx, uid, ssid)
+	if err != nil {
+		return err
+	}
+	return h.SetJWTToken(ctx, uid, ssid)
+}
+
+func (h *RedisJWTHandler) ClearToken(ctx *gin.Context) error {
+	ctx.Header("x-jwt-token", "")
+	ctx.Header("x-refresh-token", "")
+	uc := ctx.MustGet("claims").(UserClaims)
+
+	return h.client.Set(ctx,
+		fmt.Sprintf("users:ssid:%s", uc.Ssid),
+		"", h.rcExpiration).Err()
+}
+
+func (h *RedisJWTHandler) SetJWTToken(ctx *gin.Context, uid int64, ssid string) error {
+	uc := UserClaims{
+		Uid:       uid,
+		Ssid:      ssid,
+		UserAgent: ctx.GetHeader("User-Agent"),
+		RegisteredClaims: jwt.RegisteredClaims{
+			// 1 分钟过期
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Minute * 30)),
+		},
+	}
+	token := jwt.NewWithClaims(h.signingMethod, uc)
+	tokenStr, err := token.SignedString(JWTKey)
+	if err != nil {
+		return err
+	}
+	ctx.Header("x-jwt-token", tokenStr)
+	return nil
+}
+
+func (h *RedisJWTHandler) setRefreshToken(ctx *gin.Context, uid int64, ssid string) error {
+	rc := RefreshClaims{
+		Uid:  uid,
+		Ssid: ssid,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(h.rcExpiration)),
+		},
+	}
+	token := jwt.NewWithClaims(h.signingMethod, rc)
+	tokenStr, err := token.SignedString(RCJWTKey)
+	if err != nil {
+		return err
+	}
+	ctx.Header("x-refresh-token", tokenStr)
+	return nil
+}
+
+var JWTKey = []byte("k6CswdUm77WKcbM68UQUuxVsHSpTCwgK")
+var RCJWTKey = []byte("k6CswdUm77WKcbM68UQUuxVsHSpTCwgA")
+
+type RefreshClaims struct {
+	jwt.RegisteredClaims
+	Uid  int64
+	Ssid string
+}
+
+type UserClaims struct {
+	jwt.RegisteredClaims
+	Uid       int64
+	Ssid      string
+	UserAgent string
+}
---
+++ internal/web/jwt/types.go
@@ -0,0 +1,11 @@
+package jwt
+
+import "github.com/gin-gonic/gin"
+
+type Handler interface {
+	ClearToken(ctx *gin.Context) error
+	ExtractToken(ctx *gin.Context) string
+	SetLoginToken(ctx *gin.Context, uid int64) error
+	SetJWTToken(ctx *gin.Context, uid int64, ssid string) error
+	CheckSession(ctx *gin.Context, ssid string) error
+}
